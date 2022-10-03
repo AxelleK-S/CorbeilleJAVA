6c0fd90a1c875b4086c7cd621cce5ba140928b5f
User <User@AXELLE-PC>
2022-10-03 15:47:08 +0100

    Enter class Workshop

---
+++ Workshop1/src/Workshop.java
@@ -0,0 +1,13 @@
+
+public class Workshop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Personne unePersonne = new Personne();
+		unePersonne.setNom("Diet");
+		unePersonne.setPrenom("Jean-Aymeric");
+		unePersonne.direBonjour();
+
+	}
+
+}
